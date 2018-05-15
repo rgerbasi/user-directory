c2d4d71c3ba2074c16ebca2babd26d10780df0a2
Rodrigo Gerbasi <rgerbasi98@gmail.com>
2018-05-15 11:32:42 -0400

    add some commented out css to center objects, change a lot of the html, begin asking for favorite color and give a border to the name and age

--- index.js
+++ index.js
@@ -19,7 +19,24 @@ const form = document.getElementById('form');
 
 form.addEventListener('submit', function(ev){
     ev.preventDefault();
-    document.querySelector('h2').textContent = document.querySelector('input').value;
+    const userName = form.userName.value;
+    const age = form.age.value;
+    const userlist = document.querySelector('#users');
+
+    const p = document.createElement('p');
+    p.textContent = `${userName}, ${age}`;
+    //userlist.appendChild(p);
+    
+    const color = form.favoriteColor.value;
+    p.style.border = 'thick solid '+`${color}`;
+    p.style.textAlign = 'center';
+    //p.style.color = `${color}`;
+
+    userlist.appendChild(p);
+
+    form.reset();
+    form.userName.focus();
+
 });
 
 /*
